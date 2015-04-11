755b0bd8192c7ba77c6a4f1fda780579ccae0401
Yannic Klem <yann.klem@googlemail.com>
2015-04-11 19:43:58 +0200

    redirect to login on 403, too. And Bugfix on Edit-Modal

--- src/main/resources/public/authentication/HttpInterceptor.js
+++ src/main/resources/public/authentication/HttpInterceptor.js
@@ -2,8 +2,8 @@ var shopping = angular.module('shopping');
 
 shopping.service('MyHttpInterceptor',['$location', '$q', function ($location ,$q) {
     return {
-        'responseError': function(rejection) {
-            if(rejection.status == 401 || rejection.status == 0) {
+        responseError: function(rejection) {
+            if(rejection.status == 401 || rejection.status == 403 || rejection.status == 0) {
                 $location.path('/login');
             }
             return $q.reject(rejection);
