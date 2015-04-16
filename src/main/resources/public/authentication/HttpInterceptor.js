688e80df0dde45b7d3a87177db2652f312a57ee3
Yannic Klem <yann.klem@googlemail.com>
2015-04-16 19:25:57 +0200

    $cookies.remove is not a function :(

--- src/main/resources/public/authentication/HttpInterceptor.js
+++ src/main/resources/public/authentication/HttpInterceptor.js
@@ -4,8 +4,6 @@ shopping.service('MyHttpInterceptor',['$location', '$q', '$cookies', function ($
     return {
         responseError: function(rejection) {
             if(rejection.status == 401 || rejection.status == 403 || rejection.status == 0) {
-                $cookies.remove('JSESSIONID');
-                $cookies.remove('XSRF-TOKEN');
                 $location.path('/login');
             }
             return $q.reject(rejection);
